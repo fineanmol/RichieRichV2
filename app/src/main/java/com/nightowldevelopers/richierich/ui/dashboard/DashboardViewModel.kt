a996f187f64c434a51c769d180ed6ff767c56f82
Anmol <anmol.agarwal2004@yahoo.com>
2019-11-03 04:04:56 +0530

    Initial commit

---
+++ app/src/main/java/com/nightowldevelopers/richierich/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.nightowldevelopers.richierich.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is LeaderBoard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
